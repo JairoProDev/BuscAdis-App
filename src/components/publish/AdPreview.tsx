fe726b439c4331f04953551c99df8158daa6d45d
JairoProDev <JairoProDev@gmail.com>
2025-07-18 07:39:09 -0500

    ✨ Mejora en la tipificación del componente AdPreview
    - Se ha añadido la importación de la interfaz `PublicationFormData` para mejorar la tipificación del componente.
    - Se ha actualizado la definición de las propiedades del componente, asegurando una mejor gestión de los datos.
    
    ✅ Se mejoró la claridad y robustez del código en el componente AdPreview

--- src/components/publish/AdPreview.tsx
+++ src/components/publish/AdPreview.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { formatPrice } from '@/utils/format'
 import { MapPinIcon, PhoneIcon, EnvelopeIcon, TagIcon, CalendarIcon, StarIcon, EyeIcon } from '@heroicons/react/24/outline'
+import { PublicationFormData } from '@/types/publication';
 
 interface AdPreviewProps {
   ad: PublicationFormData;
